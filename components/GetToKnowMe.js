34883a1cb6a965552931963e0e5981eb7957a8d8
devkmaan <140909236+devkmaan@users.noreply.github.com>
2024-04-02 23:48:32 +0530

    GetToKnowMe.js

--- components/GetToKnowMe.js
+++ components/GetToKnowMe.js
@@ -6,10 +6,10 @@ const GetToKnowMe = () => {
     return (
         <Section delay={0.1}>
                 <Heading as="h3" variant="section-title">
-                    About Me
+                    Get to know me
                 </Heading>
                 <Text fontSize="lg" mb={7}>
-                "Thank you for visiting my portfolio website. I hope you enjoyed exploring my work and learning more about my expertise in Machine Learning Development and DSA. If you have any questions or would like to discuss potential collaborations, please feel free to reach out to me. I am always excited to connect with fellow professionals and explore new opportunities. I look forward to hearing from you."
+                    Thank you for visiting my portfolio website. I hope you enjoyed exploring my work and learning more about my expertise in Machine Learning Development and DSA. If you have any questions or would like to discuss potential collaborations, please feel free to reach out to me. I am always excited to connect with fellow professionals and explore new opportunities. I look forward to hearing from you.
                 </Text>
                 {/* <Heading as="h4" variants="section-title">On the web</Heading> */}
 
